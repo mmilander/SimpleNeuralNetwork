84a2d4b4157a405f203bcfa59a0c15f8238f1053
mitch <mitchell.milander@wsu.edu>
2025-11-14 12:34:32 -0800

    Add project files.

---
+++ SimpleNeuralNetwork/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNeuralNetwork
+{
+    public class Node
+    {
+        public Dictionary<Node, double> InputNodes { get; set; } = new Dictionary<Node, double>();
+        public double Result { get; set; } 
+
+        public Func<double,double> Sigmoid { get; set; }
+
+        public Node(Func<double,double> sigmoid, double result = 0)
+        {
+            this.Sigmoid = sigmoid;
+            this.Result = result;
+        }
+
+        public void AddInputNode(Node node)
+        {
+            InputNodes.Add(node, 0.00001);
+        }
+
+        public void CalcNode()
+        {
+            foreach(var node in InputNodes)
+            {
+
+            }
+        }
+    }
+}
